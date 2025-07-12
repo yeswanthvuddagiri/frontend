e498f3ce665fe6ca40cc672e2b7904d65afcc95c
yeswanthvuddagiri <yeswanthyeswanth20@gmail.com>
2025-07-12 10:11:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
   Link,
   Navigate
 } from 'react-router-dom';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import './App.css';
